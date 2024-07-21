6f2e048999d2cfc23ee56d0d19062695b1115639
Angelofernandez05 <jeloxfernandez@gmail.com>
2024-07-21 19:26:30 +0800

    commit

--- admin/config.php
+++ admin/config.php
@@ -1,9 +1,9 @@
 <?php
 $servername = "127.0.0.1";
 $port = "3306";
-$username = "u510162695_macwas";
+$username = "u510162695_maccwas";
 $password = "1Macwas_pass";
-$dbname = "u510162695_macwas";
+$dbname = "u510162695_maccwas";
 
 // Create connection
 $link = new mysqli($servername, $username, $password, $dbname, $port);
