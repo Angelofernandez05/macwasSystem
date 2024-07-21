9fd55303491023a114c85e45975665edd2174e6b
Angelofernandez05 <jeloxfernandez@gmail.com>
2024-07-21 19:16:30 +0800

    commit

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
-/* Database credentials */
+// Database credentials
 define('DB_SERVER', '127.0.0.1'); // Or 'localhost'
 define('DB_USERNAME', 'u510162695_macwas');
 define('DB_PASSWORD', '1Macwas_pass');
 define('DB_NAME', 'u510162695_macwas');
 
-/* Attempt to connect to MySQL database */
+// Attempt to connect to MySQL database
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 // Check connection
