f3ed5a7c76649fb5bc9449644735866af4601d3f
Angelofernandez05 <jeloxfernandez@gmail.com>
2024-07-30 17:46:45 +0800

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -11,9 +11,9 @@ $dbname = 'u510162695_macwas';
 $port = 3306;
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$link = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $link->connect_error);
 }
