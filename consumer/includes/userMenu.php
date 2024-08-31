3172943a9ac82f37be51fdcace66f5b9d0579fbc
Angelofernandez05 <jeloxfernandez@gmail.com>
2024-08-31 23:37:35 +0800

    commit

--- consumer/includes/userMenu.php
+++ consumer/includes/userMenu.php
@@ -7,23 +7,21 @@
         color: white;
     }
 </style>
-
 <div class="dropdown">
     <button class="btn btn-link dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <span style="color: white   ;">Hi, <?php echo htmlspecialchars($_SESSION["username"]); ?></span>
+        <h6 class="mb-0">Hi, <?php echo htmlspecialchars($user_row['name']); ?></h6>
     </button>
     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item" href="reset-password.php">Reset Your Password</a>
-        <a class="dropdown-item" href="generate-security.php">Generate Security Password</a>
-        <a class="dropdown-item" href="#" onclick="logoutConfirmation()">Sign Out</a>
+        <a class="dropdown-item" href="reset_password.php">Reset Your Password</a>
+        <a class="dropdown-item" href="#" onclick="confirmLogout()">Sign Out</a>
     </div>
 </div>
-
+    
 <script>
-    function logoutConfirmation() {
+    function confirmLogout() {
         Swal.fire({
             title: 'Are you sure?',
-            text: "You will be logged out!",
+            text: "You will be logged out.",
             icon: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
@@ -31,9 +29,9 @@
             confirmButtonText: 'Yes, log me out!'
         }).then((result) => {
             if (result.isConfirmed) {
-                // Redirect to logout script
                 window.location.href = 'logout.php';
             }
         });
+        return false; // Prevent the default link behavior
     }
 </script>
