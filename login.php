ed049ed2c53523f2dcb5a06da3c47a9620fe3a39
Angelofernandez05 <jeloxfernandez@gmail.com>
2024-11-18 14:31:46 +0800

    commit

--- login.php
+++ login.php
@@ -26,19 +26,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
     }
 
     // Verify reCAPTCHA
-if (empty($email_err) && empty($password_err)) {
-    $recaptcha_secret = '6Lc9TYIqAAAAAM0dTnNsu3INPMMHwQ3TM8L1obgf';
-    $recaptcha_response = isset($_POST['g-recaptcha-response']) ? $_POST['g-recaptcha-response'] : '';
-
-    if (empty($recaptcha_response)) {
-        $login_err = "Please complete the CAPTCHA verification.";
-    } else {
+    if (empty($email_err) && empty($password_err)) {
+        $recaptcha_secret = '6Lc5Dn0qAAAAAMYzPsoS20eZ8vEIEzZPE9olVTrN';
+        $recaptcha_response = $_POST['g-recaptcha-response'];
         $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptcha_secret&response=$recaptcha_response");
         $response_keys = json_decode($response, true);
 
         if (intval($response_keys["success"]) !== 1) {
-            $login_err = "CAPTCHA verification failed. Please try again.";
-
+            $login_err = "Please complete the CAPTCHA verification.";
         } else {
             // Prepare a select statement
             $sql = "SELECT id, status, password, is_approved FROM consumers WHERE email = ?";
@@ -196,7 +191,7 @@ if (empty($email_err) && empty($password_err)) {
                         </div>
 
                         <!-- Add reCAPTCHA widget -->
-                        <div class="g-recaptcha mb-3" data-sitekey="6Lc9TYIqAAAAAJEx31bPM4z8Kvae6j95r0iDRT5t"></div>
+                        <div class="g-recaptcha mb-3" data-sitekey="6LdzD30qAAAAAGGUZtHHljbEuOozmOKjwgjBJWrw"></div>
 
                         <div class="d-grid mb-3">
                             <input type="submit" value="Login" name="login" class="btn btn-primary text-light py-3">
