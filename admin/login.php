3b69e9999176b9a88d0bdc79b7ccd0f5f6f2361b
Angelofernandez05 <jeloxfernandez@gmail.com>
2024-12-09 23:28:59 +0800

    commit

--- admin/login.php
+++ admin/login.php
@@ -96,6 +96,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Close connection
     mysqli_close($link);
 }
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
+header("X-Frame-Options: SAMEORIGIN");
+header("X-Content-Type-Options: nosniff");
+header("Referrer-Policy: strict-origin-when-cross-origin");
+header("Permissions-Policy: geolocation=(self), microphone=()");
 ?>
 
 <!DOCTYPE html>
