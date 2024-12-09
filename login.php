8b0853a5e15106853ac9fe79f4ffeb307eb8ba24
Angelofernandez05 <jeloxfernandez@gmail.com>
2024-12-09 22:59:16 +0800

    commit

--- login.php
+++ login.php
@@ -93,6 +93,13 @@
         }
         mysqli_close($link);
     }
+
+    header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
+    header("X-Frame-Options: SAMEORIGIN");
+    header("X-Content-Type-Options: nosniff");
+    header("Referrer-Policy: strict-origin-when-cross-origin");
+    header("Permissions-Policy: geolocation=(self), microphone=()");
+
     ?>
 
     <!DOCTYPE html>
