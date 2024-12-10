a58ce34ce9d92ddcd14c1e7168034a16a5d00fd7
Angelofernandez05 <jeloxfernandez@gmail.com>
2024-12-10 15:08:35 +0800

    commit

--- login.php
+++ login.php
@@ -145,7 +145,7 @@ header("Permissions-Policy: geolocation=(self), microphone=()");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Consumer Login</title>
+    <title>Consumer 1Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style2.css">
     <link rel="icon" href="logo.png" type="image/icon type">
