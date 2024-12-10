b7d45a3363dd201244fe099120b9f2f582044b65
Angelofernandez05 <jeloxfernandez@gmail.com>
2024-12-10 17:59:09 +0800

    commit

--- plumber/login.php
+++ plumber/login.php
@@ -16,7 +16,7 @@ $username = $password = "";
 $username_err = $password_err = $login_err = "";
 
 // Google reCAPTCHA secret key
-// $secret_key = "6LeNVYIqAAAAAFKB4J4PHK5M3GDRb0mjkHlpxe4Y";
+$secret_key = "6LeNVYIqAAAAAFKB4J4PHK5M3GDRb0mjkHlpxe4Y";
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -232,9 +232,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                         </div>
 
                         <!-- Google reCAPTCHA -->
-                        <!-- <div class="form-outline mb-4">
+                        <div class="form-outline mb-4">
                             <div class="g-recaptcha" data-sitekey="6LeNVYIqAAAAAD8moza5cF_4G7YsCSUZjy4ZMzZi"></div>
-                        </div> -->
+                        </div>
 
                         <!-- Submit button -->
                         <div class="d-flex justify-content-center">
@@ -266,19 +266,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         this.classList.toggle("fa-eye-slash");
     });
 </script>
-<script src="https://www.google.com/recaptcha/api.js?render=6LfCwZYqAAAAAJ8wBxWCzCwsgeFpTdSYTagAmnwL" async defer></script>
 
-<script>
-    // reCAPTCHA v3
-    grecaptcha.ready(function() {
-        grecaptcha.execute('6LfCwZYqAAAAAJ8wBxWCzCwsgeFpTdSYTagAmnwL', { action: 'login' }).then(function(token) {
-            const recaptchaResponseField = document.createElement('input');
-            recaptchaResponseField.setAttribute('type', 'hidden');
-            recaptchaResponseField.setAttribute('name', 'recaptcha_response');
-            recaptchaResponseField.setAttribute('value', token);
-            document.querySelector('form').appendChild(recaptchaResponseField);
-        });
-    });
-</script>
 </body>
 </html>
