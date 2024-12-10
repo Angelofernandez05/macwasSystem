b2140917e3f9a5f311ee1a3ad90f97f8254eea43
Angelofernandez05 <jeloxfernandez@gmail.com>
2024-12-10 19:13:58 +0800

    commit

--- admin/login.php
+++ admin/login.php
@@ -15,8 +15,18 @@ require_once "config.php";
 $username = $password = "";
 $username_err = $password_err = $login_err = "";
 
-$request = $_SERVER['REQUEST_URI'];if (substr($request,-4) =='.php')
-{$new_url = substr($request,0,-4); header("Location: $new_url",true,301);exit();}
+<?php
+$request = $_SERVER['REQUEST_URI'];
+
+// Check if the URL ends with .php
+if (substr($request, -4) === '.php') {
+    // Redirect to the same URL without .php
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
+?>
+
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
