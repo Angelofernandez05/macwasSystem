b5c683b9807d2a458741eeee227e4fe503b01453
Angelofernandez05 <jeloxfernandez@gmail.com>
2024-12-10 16:53:22 +0800

    commit

--- admin/login.php
+++ admin/login.php
@@ -220,7 +220,7 @@ header("Permissions-Policy: geolocation=(self), microphone=()");
                         <div class="d-flex justify-content-center mx-2 mb-3 mb-lg-4">
                             <input type="submit" value="Sign in" name="login" class="btn btn-primary btn-lg text-light my-2 py-3 w-100" />
                         </div>
-                        <!-- <p align="center"><strong>Don't have an account? Sign up</strong><a href="register.php" class="text-primary" style="font-weight:600;text-decoration:none;"> here</a></p> -->
+                        <p align="center"><strong>Don't have an account? Sign up</strong><a href="register.php" class="text-primary" style="font-weight:600;text-decoration:none;"> here</a></p>
                     </form>
                 </div>
             </div>
