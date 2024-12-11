1bedc541d9ddeb7d922cc5959da61fb2df7740a0
Angelofernandez05 <jeloxfernandez@gmail.com>
2024-12-11 13:10:09 +0800

    commit

--- accounting/login.php
+++ accounting/login.php
@@ -236,7 +236,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                         <!-- Username input -->
                         <div class="form-outline mb-4">
                             <label class="form-label" for="form1Example13"> <i class="bi bi-person-circle"></i>  <strong>Username</strong></label>
-                            <input type="text" id="form1Example13" class="form-control form-control-lg py-3 <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo htmlspecialchars($username); ?>" name="username" autocomplete="off" placeholder="Enter username" style="border-radius:25px ;" >
+                            <input type="text" id="form1Example13" class="form-control form-control-lg py-3 <?php echo () ? 'is-invalid' : ''; ?>" value="<?php echo htmlspecialchars($username); ?>" name="username" autocomplete="off" placeholder="Enter username" style="border-radius:25px ;" >
                             <span class="invalid-feedback"><?php echo $username_err; ?></span>
                         </div>
 
