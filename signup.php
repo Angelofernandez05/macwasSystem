dcef08eda0c5e9658c671463a981bbf096ea9963
Angelofernandez05 <jeloxfernandez@gmail.com>
2024-12-11 12:02:22 +0800

    commit

--- signup.php
+++ signup.php
@@ -140,7 +140,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <img src="logo.png" alt="Admin-Icon" style="width: 200px; height: 150px;">
                 </p>
                 <h2 class="text-center mb-4 georgia-font"><strong>SIGN UP</strong></h2>
-                <form action="signup.php" method="post">
+                <form action="signup" method="post">
                     <div class="row">
                         <div class="col-md-6 col-sm-12">
                             <!-- Personal Information -->
@@ -269,7 +269,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             confirmButtonText: 'OK',
             willClose: () => {
                 // Redirect to login.php after closing the SweetAlert
-                window.location.href = "login.php";
+                window.location.href = "login";
             }
         });
     <?php } elseif ($alert_type == 'error') { ?>
