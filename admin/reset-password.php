07046201eab5e7681c931eabf14121c231e8d8ca
Angelofernandez05 <jeloxfernandez@gmail.com>
2024-12-12 15:39:26 +0800

    commit

--- admin/reset-password.php
+++ admin/reset-password.php
@@ -97,13 +97,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
             margin: auto;
             top: 130px;
         }
-        .wrapper {
-            width: 360px;
-            padding: 20px;
-            background: white;
-            border-radius: 10px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-        }
+    
     </style>
 </head>
 <body>
