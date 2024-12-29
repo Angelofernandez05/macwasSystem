edb73d79afa8bb0cfb29154b83a6b4190f7cb113
Angelofernandez05 <jeloxfernandez@gmail.com>
2024-12-29 20:46:27 +0800

    commit

--- login.php
+++ login.php
@@ -252,7 +252,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
                         </form>
                         <p class="text-center"><strong>Don't have an account? <a href="signup" class="text-primary">Sign up here</a></strong></p>
                         <p class="text-center"><strong>Forgot your password? <a href="forgot_password" class="text-primary">Click here</a></strong></p>
-                        <p class="text-center"> <strong>Approval your account call this no.<a href="tel:09755319049" class="text-primary" style="color: red;">09755319049</a></strong>
+                        <p class="text-center"> <strong>Approval your account call this no.<a href="tel:09755319049"style="color: red;">09755319049</a></strong>
                     </div>
                 </div>
             </div>
